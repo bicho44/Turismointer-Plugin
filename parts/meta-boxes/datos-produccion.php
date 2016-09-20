cfdf42f34aa45dea7799caf75659197ae9ffe778
Federico <admin@imgdigital.com.ar>
2016-09-20 15:55:26 -0300

    Meta Box Datos de Producción

---
+++ parts/meta-boxes/datos-produccion.php
@@ -0,0 +1,53 @@
+<?php
+/*
+Title: Datos de Producción
+Post Type: imgd_programa
+Description: Datos sobre la Producción del Programa
+Priority: default
+Order: 3
+*/
+
+piklist (
+    'field',
+    array(
+        'type' => 'number',
+        'scope' => 'post_meta',
+        'field' => 'imgd_programa_ano',
+        'label' => __('Año', 'imgd'),
+        'value' => 2016,
+        'attributes' => array(
+            'class' => 'text'
+        ),
+        'position' => 'wrap'
+    )
+);
+
+piklist (
+    'field',
+    array(
+        'type' => 'text',
+        'scope' => 'post_meta',
+        'field' => 'imgd_programa_director',
+        'label' => __('Director', 'imgd'),
+        'value' => 'Carlos Snaimon',
+        'attributes' => array(
+            'class' => 'text'
+        ),
+        'position' => 'wrap'
+    )
+);
+
+piklist (
+    'field',
+    array(
+        'type' => 'text',
+        'scope' => 'post_meta',
+        'field' => 'imgd_programa_director',
+        'label' => __('Producción', 'imgd'),
+        'value' => 'Filmarte',
+        'attributes' => array(
+            'class' => 'text'
+        ),
+        'position' => 'wrap'
+    )
+);
