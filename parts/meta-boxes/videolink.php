856c9806f2bdddb11f79f5e0f6a664d21d1a5fcb
Federico <admin@imgdigital.com.ar>
2016-09-20 13:43:07 -0300

    Primeros pasos para que se pueda usar el Thumbnail de video

--- parts/meta-boxes/videolink.php
+++ parts/meta-boxes/videolink.php
@@ -28,7 +28,29 @@ piklist('field', array(
     ,'field' => '_thumbnail_id' // Use these field to match WordPress featured images.
     ,'scope' => 'post_meta'
     ,'options' => array(
-        'title' => __('Imagenes del Show', 'imgd')
-        ,'button' => __('Inserte las imagenes', 'imgd')
+        'title' => __('Imagen del Programa', 'imgd')
+        ,'button' => __('Inserte la imagen', 'imgd')
     )
 ));
+
+$uploaddir = wp_upload_dir();
+$uploadfile = $uploaddir['path'] . '/' . $filename;
+
+$contents= file_get_contents($uploadfile);
+$savefile = fopen($uploadfile, 'w');
+fwrite($savefile, $contents);
+fclose($savefile);
+?>
+<script type="text/javascript">
+
+jQuery(document).ready(function( $ ) {
+
+  $("#_post_meta_imgd_programa_video_0").on('change',function () {
+     var ytl = $( "#_post_meta_imgd_programa_video_0" ).val();
+     var yti = ytl.match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/);
+     $('_post_meta__thumbnail_id_0').value("http://i3.ytimg.com/vi/"+ yti[1] + "/hqdefault.jpg");
+     //$( "#msg" ).html( "<img src=\"http://i3.ytimg.com/vi/" + yti[1] + "/hqdefault.jpg\" /><input name=\"imageURL\" id=\"copyimageURL\" class=\"text\" type=\"text\" value=\"http://i3.ytimg.com/vi/"+ yti[1] + "/hqdefault.jpg\" onclick=\"this.select()\" readonly />" );
+  });
+
+});
+</script>
